64945804ba5da48ffb3f1d4a8bcee1cca304747f
waterblue13 <waterblue13@gmail.com>
2018-05-30 15:21:28 +0800

    Update test_mountaincar.py

--- test_mountaincar.py
+++ test_mountaincar.py
@@ -1,12 +1,13 @@
 import gym
+from algorithms import HeuristicDiscreteExploration, GraphSearch
 
 def main():
   # initialization
   env_name = "MountainCar-v0"
   random_seed = 1234
   num_episodes = 100
-  exploration_policy = HeuristicDiscreteExploration()
-  learning_policy = GraphSearch()
+  exploration_policy = HeuristicDiscreteExploration(state_dim = env.observation_space.shape[0], ac_dim = env.action_space.n)
+  learning_policy = GraphSearch(state_dim = env.observation_space.shape[0], ac_dim = env.action_space.n)
   
   # training
   env = gym.make(env_name)
@@ -16,8 +17,8 @@ def main():
     while true:
       action = exploration_policy.take_action(state)
       next_state, reward, done, _ = self.env.step(action)
-      exploration_policy.update(state, action, reward, next_state)
-      learning_policy.update(state, action, reward, next_state)
+      exploration_policy.update(state, action, reward, next_state, done)
+      learning_policy.update(state, action, reward, next_state, done)
       next_state = state
       if done: break
       
@@ -32,4 +33,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-  
